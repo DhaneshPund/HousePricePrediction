c3ba154522928fedb69faa1f94e4d45ce35ea80c
DhaneshPund <60513168+DhaneshPund@users.noreply.github.com>
2020-03-19 19:31:11 +0530

    web-service for price prediction

--- app.py
+++ app.py
@@ -20,4 +20,21 @@ def train():
 
     return 'Model has been Trained'
 
+@app.route('/test', methods=['POST'])
+def test():
+    clf = joblib.load('filename.pkl')
+    request_data = request.get_json()
+    a = request_data['Avg. Area Income']
+    b = request_data['Avg. Area House Age']
+    c = request_data['Avg. Area Number of Rooms']
+    d = request_data['Avg. Area Number of Bedrooms']
+    e = request_data['Area Population']
+    l = [a, b, c, d, e]
+    narr = np.array(l)
+    narr = narr.reshape(1, 5)
+    df_test = pd.DataFrame(narr, columns=['Avg. Area Income', 'Avg. Area House Age', 'Avg. Area Number of Rooms','Avg. Area Number of Bedrooms', 'Area Population'])
+    ypred = clf.predict(df_test)
+    result = ypred.tolist() #as ndarray object cannot be converted directly to json
+    return jsonify({'Price': result})
+
 app.run(port=5000)
